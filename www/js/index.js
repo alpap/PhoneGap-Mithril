bde3d36f622efc8ec8468efeba5e07377a406ed5
Alexandros Papadopoulos <alpa@hesehus.dk>
2018-11-02 07:22:00 +0100

    added phonegap js config

--- www/js/index.js
+++ www/js/index.js
@@ -1,99 +1,49 @@
-var root = document.body;
-var count = 0; // added a variable
-var Splash = {
-	view: function() {
-		return m('div', { class: 'menu' }, [
-			m('a', { class: 'button _round', href: '#!/click' }, '1 click demo'),
-			m('br'),
-			m('a', { class: 'button _round', href: '#!/request' }, 'Joke'),
-		]);
-	},
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
 };
-var Click = {
-	view: function() {
-		return m('main', { class: 'center ' }, [
-			m('h1', { class: 'mar-bot' }, 'Counter'),
-			// changed the next line
-			m('h2', { class: 'white' }, count),
-			m(
-				'button',
-				{
-					class: 'button _round',
-					onclick: function() {
-						count++;
-					},
-				},
-				' increment',
-			),
-			m('br'),
-			m(
-				'button',
-				{
-					class: 'button _round',
-					onclick: function() {
-						count--;
-					},
-				},
-				' decrement',
-			),
-			m('div', { class: 'bottom-menu' }, [
-				m('a', { class: 'button _round ', href: '#!/splash' }, 'back'),
-			]),
-		]);
-	},
-};
-var count2 = 0;
-var jokes = [];
-var getJoke = name => {
-	m.request({
-		method: 'GET',
-		url: 'http://api.icndb.com/jokes/random?firstname=' + name,
-		data: {},
-		withCredentials: false,
-	}).then(function(data) {
-		jokes.push(data.value.joke);
-	});
-};
-// component
-var jokesComponent = {
-	view: function(vnode) {
-		let jokelist = vnode.attrs.list;
-		if (jokelist.length <= 0) return;
-		let mjokes = [];
-		jokes.forEach(joke =>
-			mjokes.unshift(
-				m(
-					'h4',
-					{
-						style:
-							'margin: 10px 15px 15px 15px; color:' +
-							randomColor({
-								luminosity: 'light',
-								hue: 'random',
-							}),
-					},
-					joke,
-				),
-			),
-		);
-		return mjokes.slice(0, 7);
-	},
-};
-var Request = {
-	view: () => {
-		return m('main', { class: 'center' }, [
-			m('div', { class: 'title' }, [m('h1', { class: 'p-top-e ' }, 'Chucks best jokes')]),
-			m(jokesComponent, { class: 'pushdown', list: jokes }),
-			m('div', { class: 'bottom-menu' }, [
-				m('button', { class: 'button _round mar-top', onclick: getJoke }, 'Get chuck joke'),
-				m('br'),
-				m('a', { class: 'button _round ', href: '#!/splash' }, 'back'),
-			]),
-		]);
-	},
-};
-m.route(root, '/splash', {
-	'/splash': Splash,
-	'/click': Click,
-	'/request': Request,
-});
